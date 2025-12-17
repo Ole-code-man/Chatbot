2fe65df91c73bf6f4fc856d846899aa26ac0d11a
Ole-code-man <ole.magnus.letrud@gmail.com>
2025-12-17 15:52:04 +0100

    added post figure to select historical figure

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 # paste "uvicorn app.main:app --reload" to run website
@@ -6,7 +6,14 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class ChatRequest (BaseModel):
+    figure: str
     message: str
+
+FIGURE_RESPONSES = {
+    "napoleon": "I am Napoleon Bonaparte, Emperor of the French.",
+    "cleopatra": "I am Cleopatra, Queen of Egypt.",
+    "caesar": "I am Gaius Julius Caesar, dictator of Rome."
+}
 
 @app.get("/ping")
 def ping():
@@ -14,6 +21,14 @@ def ping():
 
 @app.post("/chat")
 def chat(request: ChatRequest):
+    figure = request.figure.lower()
+
+    if figure not in FIGURE_RESPONSES:
+        raise HTTPException(
+            status_code=400,
+            detail = f"Unknown historical figure: {request.figure}"
+        )
+    base_response = FIGURE_RESPONSES[figure]
     return{
-        "reply" : f"You said: {request.message}"
+        "reply" : f"{base_response} You asked: {request.message}"
     }
