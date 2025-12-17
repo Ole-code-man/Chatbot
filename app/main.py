75ded770e645614bcb4c9c51ec0e46091bfe7a28
Ole-code-man <ole.magnus.letrud@gmail.com>
2025-12-17 13:12:02 +0100

    added post chat

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+# paste "uvicorn app.main:app --reload" to run website
+
 app = FastAPI()
+
+class ChatRequest (BaseModel):
+    message: str
 
 @app.get("/ping")
 def ping():
     return {"status":"ok"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    return{
+        "reply" : f"You said: {request.message}"
+    }
